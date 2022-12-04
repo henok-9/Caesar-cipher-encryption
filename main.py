64841154a322c0027e3c55782b6e27908bff60ba
henok-9 <henokabe89@gmail.com>
2022-12-04 02:27:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-
-# Path: caesar.py
 #encrypt the message using the caesar cipher
 def encrypt(message, key):
     encrypted_message = ""
     for ch in message:
         encrypted_message += chr(ord(ch) + key)
     return encrypted_message
+
+
 #decrypt the message using the caesar cipher
 def decrypt(message, key):
     decrypted_message = ""
     for ch in message:
         decrypted_message += chr(ord(ch) - key)
     return decrypted_message
-
-#
 
 
 #get input from user and encrypt it using the caesar cipher
@@ -24,5 +22,7 @@ def main():
     print("Encrypted message: ", encrypted_message)
     decrypted_message = decrypt(encrypted_message, key)
     print("Decrypted message: ", decrypted_message)
+    
+    
 #call the main function
 main()
